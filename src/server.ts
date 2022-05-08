d0dfe6a16089916ce92c300c0acc26dce344fb61
Vinicius Audibert <vini.audibert@gmail.com>
2022-05-07 23:40:32 -0300

    feat: adicionar cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes'
 
 const port = process.env.PORT || 3001
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
